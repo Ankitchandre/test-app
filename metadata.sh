b46f6383c38756436bcc865608130daac02b481c
Ankitchandre <ankitchandre9@gmail.com>
2021-09-17 12:31:57 +0530

    Update metadata.sh

--- metadata.sh
+++ metadata.sh
@@ -1,2 +1,6 @@
-#!/usr/bin/env bash
-export tagvalue=$GO_REVISION_GIT
+#! /usr/bin/env bash
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cat > $SOURCE_DIR/../metadata <<EOF
+export HELM_TAG=$GO_REVISION
+EOF
